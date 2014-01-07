cee4a72f5e66199634e8b78fc0e4b46cffce0f20
Kriss <kriss@XIXs.com>
2014-01-07 21:41:35 +0000

    .

--- ctrack/src/ctrack.html.js
+++ ctrack/src/ctrack.html.js
@@ -15,7 +15,48 @@ ctrack.setup_html=function()
 
 	ctrack.div.main.html( ctrack.plate.chunk("loading",{})  );
 	
-	ctrack.fetch({});
+//	ctrack.fetch({});
+
+	var d={};
+	
+	var chunk=function(n){
+		d[n]=ctrack.plate.chunk(n,d);
+	}
+	
+	chunk("ctend");
+	chunk("ctplan");
+	chunk("ctabout");
+	chunk("ctlogo");
+	chunk("ctembed");
+	
+	chunk("ctactive");
+	chunk("ctactivities");
+	chunk("ctpublishers");
+	chunk("ctbox2");
+	chunk("ctbox2table");
+	chunk("ctbox2more");
+	chunk("ctbox3");
+	chunk("ctbox3table");
+	chunk("ctbox2more");
+	chunk("ctnearhead");
+	chunk("ctneartable");
+	chunk("ctnearmore");
+	chunk("ctfootboxes");
+
+	chunk("ctnav");
+	chunk("cthead");
+	chunk("ctbox1");
+	chunk("ctbox1table");
+	chunk("ctbox1more");
+	chunk("ctboxes");
+	chunk("ctnear");
+	chunk("ctfooter");
+	chunk("ctfind");
+	chunk("bodytest");
+ 
+	ctrack.div.main.html( d.bodytest );
+ 
+
 
 }
 
