63e94e60995abc5edae678cdc433d2b88d0608a8
Kriss <kriss@XIXs.com>
2014-01-22 04:13:07 +0000

    .

--- bin/update.sh
+++
@@ -1,9 +0,0 @@
-cd `dirname $0`
-
-wget http://dl.google.com/closure-compiler/compiler-latest.zip
-unzip compiler-latest.zip compiler.jar
-rm compiler-latest.zip
-
-rm gamecake
-wget https://bitbucket.org/xixs/bin/raw/tip/exe/gamecake
-chmod +x gamecake
--- ctrack/bake.js
+++ ctrack/bake.js
@@ -1,8 +1,53 @@
 var sys = require('sys')
 var child_process = require('child_process');
+var lazy = require("lazy");
+var fs = require("fs");
+
+
+
+
+var text = fs.readFileSync("chunks/base.html",'utf8');
+
+var chunk;
+var chunks={};
+
+text.split("\n").forEach(function(l){
+		if(l[0]=="#")
+		{
+			if(l[1]!="#")
+			{
+				var name=l.substring(1).replace(/\s+/g, " ").split(" ")[0];
+				if(name)
+				{
+					chunk=chunks[name];
+					if(!chunk)
+					{
+						chunk=[];
+						chunks[name]=chunk;				
+					}
+				}
+			}
+		}
+		else
+		if(chunk)
+		{
+			chunk.push(l);
+		}
+	});
+
+for( n in chunks )
+{
+	chunks[n]=chunks[n].join("\n");
+}
+
+fs.writeFileSync("js/ctrack.chunks.js","exports.chunks="+JSON.stringify(chunks)+";");
+
+
+console.log("Parsed chunks/base.html into js/ctrack.chunks.js")
+
 
 var exec=function(s,f){
-	console.log(s);
+//	console.log(s);
 	child_process.exec(s, function(error, stdout, stderr) {
 		sys.puts(stdout);
 		if(f) { f(null,stdout); }
@@ -17,6 +62,10 @@ var jsfiles=[
 "js/ctrack.json.js"];
 
 
+console.log("Creating js/ctrack.max.js")
 exec("node_modules/uglify-js/bin/uglifyjs "+jsfiles.join(" ")+" --wrap ctrack -b -o js/ctrack.max.js");
+
+console.log("Creating js/ctrack.min.js")
 exec("node_modules/uglify-js/bin/uglifyjs "+jsfiles.join(" ")+" --wrap ctrack -c -m -o js/ctrack.min.js");
 
+console.log("All done.")
