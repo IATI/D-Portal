a8ffc34d05d4eeceab44a38449acf06703e5202b
kriss <kriss@xixs.com>
2014-03-14 12:49:03 +0000

    .

--- ctrack/js/serv.js
+++ ctrack/js/serv.js
@@ -19,8 +19,8 @@ app.use(express.json());
 
 app.use( function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
