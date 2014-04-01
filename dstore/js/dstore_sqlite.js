71186baa454df9fbe1826064989e5dde574a6cb3
kriss <kriss@xixs.com>
2014-04-01 10:23:56 +0100

    try use the db cache

--- dstore/js/dstore_sqlite.js
+++ dstore/js/dstore_sqlite.js
@@ -16,12 +16,12 @@ var sqlite3 = require("sqlite3").verbose();
 var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
 
 dstore_sqlite.close = function(db){
-	db.close();
+//	db.close();
 };
 
 dstore_sqlite.open = function(){
-//	var db = new sqlite3.cached.Database( global.argv.database );
-	var db = new sqlite3.Database( global.argv.database );
+	var db = new sqlite3.cached.Database( global.argv.database );
+//	var db = new sqlite3.Database( global.argv.database );
 	
 // speed up data writes.
 	db.serialize(function() {
