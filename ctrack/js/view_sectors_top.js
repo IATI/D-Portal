e412c41b1317e6e7177362743668a069822671a7
kriss <kriss@xixs.com>
2014-04-03 21:27:25 +0100

    fix top sectors donut chart

--- ctrack/js/view_sectors_top.js
+++ ctrack/js/view_sectors_top.js
@@ -30,85 +30,70 @@ view_sectors_top.ajax=function(args)
 	args=args || {};
 	var limit=args.limit || 5;
 
-	var year=2012;
-	var dat={
-			"from":"trans,country,sector",
-			"limit":-1,
-			"select":"sector_group,sum_of_percent_of_trans_usd",
-			"groupby":"1",
-			"orderby":"2-",
-			"code":"D|E",
-			"trans_day_gteq":year+"-01-01","trans_day_lt":(parseInt(year)+1)+"-01-01",
-			"country_code":(args.country || ctrack.args.country)
-		};
+	var list=[];
+// insert crs data if we have it
+	var crs=crs_year_sectors[ (args.country || ctrack.args.country).toUpperCase() ];
+	for(var n in crs)
+	{
+		if(n!="Grand Total")
+		{
+			var d={};
+			d.sector_group=n;
+			d.usd=crs[n];
+			list.push(d);
+		}
+	}
+	list.sort(function(a,b){
+		return ( (b.usd||0)-(a.usd||0) );
+	});
 
-	var callback=function(data){
-//		console.log("fetch transactions sectors "+year);
-//		console.log(data);
-
-		var total=0; data.rows.forEach(function(it){
-			if(it.sum_of_percent_of_trans_usd>0)
+	var total=0; list.forEach(function(it){
+		if(it.usd>0)
+		{
+			total+=it.usd;
+		}
+		else
+		{
+			total-=it.usd;
+		}
+	});
+	var shown=0;
+	var dd=[];
+	for( var i=0; i<limit ; i++ )
+	{
+		var v=list[i];			
+		if(v)
+		{
+			if((i==limit-1)&&(i<(list.length-1))) // last one combines everything else
 			{
-				total+=it.sum_of_percent_of_trans_usd;
+				v={};
+				v.usd=Math.floor(total-shown);
+				v.sector_group=(1+list.length-limit)+" More";
 			}
 			else
 			{
-				total-=it.sum_of_percent_of_trans_usd;
+				v.usd=Math.floor(v.usd);
 			}
-		});
-		var shown=0;
-		var dd=[];
-		for( var i=0; i<limit ; i++ )
-		{
-			var v=data.rows[i];			
+			
 			if(v)
 			{
-				if((i==limit-1)&&(i<(data.rows.length-1))) // last one combines everything else
-				{
-					v={};
-					v.usd=Math.floor(total-shown);
-					v.sector_group=(1+data.rows.length-limit)+" More";
-				}
-				else
-				{
-					v.usd=Math.floor(v.sum_of_percent_of_trans_usd);
-				}
-				
-				if(v)
-				{
-					var d={};
-					d.num=v.usd;
-					if(d.num<0) { d.num=-d.num; }
-					shown+=d.num;
-					d.pct=Math.floor(100*d.num/total);
-					d.str_num=commafy(v.usd)+" USD";
-					d.str_lab=v.sector_group;
-					d.str=d.str_lab+" ("+d.pct+"%)"+"<br/>"+d.str_num;
-					dd.push(d);
-				}
+				var d={};
+				d.num=v.usd;
+				if(d.num<0) { d.num=-d.num; }
+				shown+=d.num;
+				d.pct=Math.floor(100*d.num/total);
+				d.str_num=commafy(v.usd)+" USD";
+				d.str_lab=v.sector_group;
+				d.str=d.str_lab+" ("+d.pct+"%)"+"<br/>"+d.str_num;
+				dd.push(d);
 			}
 		}
+	}
 
-/*
-		var s=[];
-		var t;
-		for(var i=0;i<data.rows.length;i++)
-		{
-			var v=data.rows[i];
-			var n=v.sum_of_percent_of_trans_usd;
-			t=t||n;
-			v.usd=commafy(""+Math.floor(v.sum_of_percent_of_trans_usd));
-			v.pct=Math.floor(100*n/t);
-			s.push( plate.replace("{main_sector_row}",v) );
-		}
-		
-		ctrack.chunk("main_sector_rows",s.join(""));
-*/
+	ctrack.chunk("data_chart_sectors",dd);
+	
+	ctrack.display();
 
-		ctrack.chunk("data_chart_sectors",dd);
-		
-		ctrack.display();
-	};
 	fetch.ajax(dat,callback);
 
 }
