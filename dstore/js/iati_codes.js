3ea457f26d19a289a50ae9153b71d7d0594f56f6
kriss <kriss@xixs.com>
2014-04-04 21:03:37 +0100

    more codes, list of valid publisher ids

--- dstore/js/iati_codes.js
+++ dstore/js/iati_codes.js
@@ -55,37 +55,8 @@ iati_codes.fetch = function(){
 
 	var codes={};
 	var publishers={};
+	var packages={};
 
-if(true)
-{
-	codes.publisher_ids={};
-
-	var js=wait.for(http_getbody,"http://iatiregistry.org/api/rest/group");
-	var j=JSON.parse(js);
-	j.forEach(function(v){
-		console.log(v);
-		var jjs=wait.for(http_getbody,"http://iatiregistry.org/api/rest/group/"+v);
-		var jj=JSON.parse(jjs);
-		publishers[v]=jj
-		
-		var ids=jj.extras.publisher_iati_id.split("|");
-		for(var i=0;i<ids.length;i++)
-		{
-			var id=ids[i].trim();
-			if(id!="")
-			{
-				codes.publisher_ids[ id ]=v;
-			}
-		}
-	});
-	
-//	ls(publishers);
-
-	console.log("Writing json/publishers.json")
-	fs.writeFileSync(__dirname+"/../json/publishers.json",JSON.stringify(publishers,null,'\t'));
-
-//	return;
-}	
 	var files=[
 			{
 				url:"http://dev.iatistandard.org/_static/codelists/json/en/Sector.json",
@@ -382,6 +353,67 @@ if(true)
 //	fs.writeFileSync("json/crs_2012.js","exports.crs="+JSON.stringify(o)+";\n");
 	fs.writeFileSync(__dirname+"/../json/crs_2012_sectors.json",JSON.stringify(o,null,'\t'));
 
+
+console.log("************************ This next bit takes a loooooong time, feel free to just give up and ctrl C it, I wont judge ypu and all the important stuff is done...");
+if(true)
+{
+	codes.publisher_ids={};
+
+	var js=wait.for(http_getbody,"http://iatiregistry.org/api/rest/group");
+	var j=JSON.parse(js);
+	j.forEach(function(v){
+		console.log("Fetching publisher info for "+v);
+		var jjs=wait.for(http_getbody,"http://iatiregistry.org/api/rest/group/"+v);
+		var jj=JSON.parse(jjs);
+		publishers[v]=jj
+		
+		var ids=jj.extras.publisher_iati_id.split("|");
+		for(var i=0;i<ids.length;i++)
+		{
+			var id=ids[i].trim();
+			if(id!="")
+			{
+				codes.publisher_ids[ id ]=v;
+			}
+		}
+	});
+	
+//	ls(publishers);
+
+	console.log("Writing json/publishers.json")
+	fs.writeFileSync(__dirname+"/../json/publishers.json",JSON.stringify(publishers,null,'\t'));
+
+//
+// use the following query
+//
+// http://iatiregistry.org/api/search/dataset?fl=name,download_url,metadata_modified,groups,id,data_updated&offset=0&limit=99999
+// actually that does not work
+// try
+// http://iatiregistry.org/api/search/dataset?fl=name,res_url&offset=0&limit=99999
+//
+
+// get download urls of each xml file ?
+/*
+	for(var pname in publishers)
+	{
+		var p=publishers[pname];
+		for(var i=0;i<p.packages.length;i++)
+		{
+			var name=p.packages[i];
+			console.log("Fetching package info for "+name);
+			var js=wait.for(http_getbody,"http://iatiregistry.org/api/rest/dataset/"+name);
+			var j=JSON.parse(js);
+			packages[name]=j.download_url; // the only useful value...
+		}
+	}
+
+// list of urls if we want to do our own scan...
+	console.log("Writing json/packages.json")
+	fs.writeFileSync(__dirname+"/../json/packages.json",JSON.stringify(packages,null,'\t'));
+
+//	return;
+*/
+}	
 
 }
 
