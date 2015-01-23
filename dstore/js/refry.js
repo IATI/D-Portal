468ae7a755e9b6dd60ae623e7612c01084a48d06
kriss <kriss@xixs.com>
2015-01-23 03:03:13 +0000

    IATI 201 bump but savi is broken

--- dstore/js/refry.js
+++ dstore/js/refry.js
@@ -177,19 +177,16 @@ refry.tagval_trim=function(json,name)
 	}
 }
 
-// as above but prefer first narrative subtag if it exists
+// as above but prefer *english* narrative subtag if it exists 
 refry.tagval_narrative=function(json,name)
 {
-	var t=refry.tag(json,name); // find
+	var t=refry.tag(json,name); // find first
 	if(t)
 	{
-		var n=refry.tagval_trim(t,"narrative");
+		var n=refry.tagval_en(t,"narrative");
 		if(n) { return n; }
 	}
-	if( t && t[1] && t[1][0] && ( "string" == typeof t[1][0] ) ) // check
-	{
-		return entities.decodeXML(t[1][0].trim());
-	}
+	return refry.tagval_en(json,name);
 }
 
 // return the attr string of the first tag we find of the given name
@@ -255,7 +252,7 @@ refry.tagval_en=function(json,name)
 		{
 			if(it[0]==name) {
 				var l=it["xml:lang"]; if(l) { l=l.toLowerCase(); }
-				if((!ret_en)&&(l=="en")) { ret=it; ret_en=it } // the first english tag we found
+				if((!ret_en)&&((l=="en")||(!l))) { ret=it; ret_en=it } // the first english tag we found
 				else if(!ret) { ret=it; } // the first tag we found
 			}
 			if(!ret) // only recurse if not found anything at this level yet
@@ -270,6 +267,6 @@ refry.tagval_en=function(json,name)
 	var t=ret;
 	if( t && t[1] && t[1][0] && ( "string" == typeof t[1][0] ) ) // check
 	{
-		return entities.decodeXML(t[1][0]);
+		return entities.decodeXML(t[1][0].trim());
 	}
 }
