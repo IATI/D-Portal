8170878b4784b3acc4c57fd76ca3a9a75b059335
kriss <kriss@xixs.com>
2015-09-26 14:23:55 +0100

    sameas last commit but for pin map

--- ctrack/js/view_map.js
+++ ctrack/js/view_map.js
@@ -324,8 +324,8 @@ view_map.ajax_pins=function(args)
 //			"reporting_ref":(args.publisher || ctrack.args.publisher_select),
 //			"title_like":(args.search || ctrack.args.search),
 		};
-	if(dat.country_code) { /*dat.from+=",country";*/ dat.country_percent=100; }
 	fetch.ajax_dat_fix(dat,args);
+	if(dat.country_code) { /*dat.from+=",country";*/ dat.country_percent=100; }
 		
 	fetch.ajax(dat,args.callback || function(data)
 	{
