02958b951368bb57324b19084779b3148085ffed
kriss <kriss@xixs.com>
2016-06-14 21:06:13 +0100

    setup vagrant postgres

--- vbox/provision.sh
+++ vbox/provision.sh
@@ -13,6 +13,28 @@ sudo -u vagrant -H bash -c "byobu-enable"
 
 echo " install node npm nvm "
 
-sudo -u vagrant -H bash -c " wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash "
-sudo -u vagrant -H bash -c " . ~/.nvm/nvm.sh ; nvm install stable ; nvm use stable "
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install stable
+nvm use stable
+
+n=$(which node)
+n=${n%/bin/node}
+chmod -R 755 $n/bin/*
+sudo cp -r $n/{bin,lib,share} /usr/local
+
+
+echo " attempting to setup postgres "
+
+pgmain=/etc/postgresql/9.1/main
+
+echo '#HAXTBH' >> $pgmain/postgresql.conf
+echo 'max_wal_senders=1' >> $pgmain/postgresql.conf
+echo 'wal_level=hot_standby' >> $pgmain/postgresql.conf
+
+echo '#HAXTBH' >> $pgmain/pg_hba.conf
+echo 'local replication all peer' >> $pgmain/pg_hba.conf
+/etc/init.d/postgresql restart
+
+sudo -u postgres bash -c "psql -c \"CREATE USER vagrant WITH SUPERUSER PASSWORD 'vagrant';\""
 
