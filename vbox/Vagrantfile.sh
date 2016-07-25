e047d74ed88e76e9da616e81118973c3e71c28b0
kriss <kriss@xixs.com>
2016-07-25 20:34:36 +0100

    escapeing issues makes it better to split the provisioning into its own file

---
+++ vbox/Vagrantfile.sh
@@ -0,0 +1,66 @@
+
+sudo apt-get update
+
+echo " install build system "
+
+sudo apt-get install -y gcc-5 g++-5
+sudo apt-get install -y build-essential
+
+
+
+echo " install postgres "
+
+sudo apt-get install -y postgresql postgresql-contrib
+
+sudo pg_dropcluster --stop 9.5 main
+sudo pg_createcluster --locale en_US.UTF-8 --start 9.5 main
+
+
+echo " install and enable byobu "
+
+sudo apt-get install -y byobu
+sudo -u vagrant -H bash -c "byobu-enable"
+
+
+echo " hard install node npm nvm"
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | NVM_DIR=/usr/local/nvm PROFILE=/etc/bash.bashrc bash
+. /usr/local/nvm/nvm.sh
+nvm install 6
+nvm use 6
+
+# create node executable
+echo "#!/bin/bash
+export NVM_DIR=\"/usr/local/nvm\"
+[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
+node \$@" > /usr/local/bin/node
+chmod +x /usr/local/bin/node
+
+# create npm executable
+echo "#!/bin/bash
+export NVM_DIR=\"/usr/local/nvm\"
+[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
+npm \$@" > /usr/local/bin/npm
+chmod +x /usr/local/bin/npm
+
+
+echo " attempting to setup postgres "
+
+PGMAIN=/etc/postgresql/9.5/main
+PGUSER=vagrant
+PGPASS=vagrant
+
+echo '#HAXTBH' >> $PGMAIN/postgresql.conf
+echo 'max_wal_senders=1' >> $PGMAIN/postgresql.conf
+echo 'wal_level=hot_standby' >> $PGMAIN/postgresql.conf
+
+echo '#HAXTBH' >> $PGMAIN/pg_hba.conf
+echo 'local replication all peer' >> $PGMAIN/pg_hba.conf
+/etc/init.d/postgresql restart
+
+sudo -u postgres bash -c "psql -c \"CREATE USER $PGUSER WITH SUPERUSER PASSWORD '$PGPASS';\""
+
+sudo -u postgres createdb dstore
+sudo -u postgres bash -c "psql -c \"CREATE EXTENSION IF NOT EXISTS citext;\" dstore "
+sudo -u postgres psql -l
+
