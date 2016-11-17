7326eb39718397d4ea94fe9a635d7e9b49838afb
kriss <kriss@xixs.com>
2016-11-17 09:59:44 +0000

    fix single aid view when search filters confuse it

--- ctrack/js/view_act.js
+++ ctrack/js/view_act.js
@@ -55,30 +55,22 @@ view_act.ajax=function(args)
 			"aid":args.aid,
 		};
 
-	if(args.lat)
+
+	if(!dat.aid) // only filter when not asking for a single aid
 	{
-		dat.location_latitude_gt=Number(args.lat)-0.001;
-		dat.location_latitude_lt=Number(args.lat)+0.001;
+		if(args.lat)
+		{
+			dat.location_latitude_gt=Number(args.lat)-0.001;
+			dat.location_latitude_lt=Number(args.lat)+0.001;
+		}
+		if(args.lng)
+		{
+			dat.location_longitude_gt=Number(args.lng)-0.001;
+			dat.location_longitude_lt=Number(args.lng)+0.001;
+		}
+		fetch.ajax_dat_fix(dat,args);
 	}
-	if(args.lng)
-	{
-		dat.location_longitude_gt=Number(args.lng)-0.001;
-		dat.location_longitude_lt=Number(args.lng)+0.001;
-	}
-//	for(var n in ctrack.q) { dat[n]=ctrack.q[n]; }
-//	for(var n in ctrack.hash) { dat[n]=ctrack.hash[n]; }
-//	for(var n in args.q) { dat[n]=args.q[n]; }
-//	if(dat.country_code) { dat.from+=",country"; }
-//	if(dat.location_latitude && dat.location_longitude) { dat.from+=",location"; }
-	fetch.ajax_dat_fix(dat,args);
-	if(dat.aid){
-		delete dat.location_latitude_gt;
-		delete dat.location_latitude_lt;
-		delete dat.location_longitude_gt;
-		delete dat.location_longitude_lt;
-		delete dat.country_code;
-		delete dat.reporting_ref;
-	}
+	
 	fetch.ajax(dat,args.callback || function(data)
 	{
 //		console.log("view_act.numof_callback");
