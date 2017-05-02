7adbceb9df20b2781eecd4d01972d095cf0e5f22
kriss <kriss@xixs.com>
2017-05-02 22:12:05 +0100

    change box and use apt nodejs

--- vbox/Vagrantfile.sh
+++ vbox/Vagrantfile.sh
@@ -6,7 +6,7 @@ echo " install build system "
 sudo apt-get install -y gcc-5 g++-5 build-essential
 
 echo " setting default DSTORE_PG , must be done before byobu breaks .profile "
-echo "export DSTORE_PG=\"/var/run/postgresql dstore\" " >> ~/.profile
+echo "export DSTORE_PG=\"/var/run/postgresql dstore\" " >> /home/vagrant/.profile
 
 echo " install and enable byobu "
 
@@ -15,39 +15,43 @@ sudo -u vagrant -H bash -c "byobu-enable"
 
 
 
-echo " hard install node npm nvm"
+echo " apt install node npm"
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | NVM_DIR=/usr/local/nvm PROFILE=/etc/bash.bashrc bash
-. /usr/local/nvm/nvm.sh
-nvm install default # yeah but this node seems to get broken less for some reason?
-nvm use default
+sudo apt-get install -y nodejs-legacy nodejs npm
+
+#echo " hard install node npm nvm"
+
+#wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | NVM_DIR=/usr/local/nvm PROFILE=/etc/bash.bashrc bash
+#. /usr/local/nvm/nvm.sh
+#nvm install default # yeah but this node seems to get broken less for some reason?
+#nvm use default
 
 # create node executable
-echo "#!/bin/bash
-export NVM_DIR=\"/usr/local/nvm\"
-[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
-node \$@" > /usr/local/bin/node
-chmod +x /usr/local/bin/node
+#echo "#!/bin/bash
+#export NVM_DIR=\"/usr/local/nvm\"
+#[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
+#node \$@" > /usr/local/bin/node
+#chmod +x /usr/local/bin/node
 
 # create npm executable
-echo "#!/bin/bash
-export NVM_DIR=\"/usr/local/nvm\"
-[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
-npm \$@" > /usr/local/bin/npm
-chmod +x /usr/local/bin/npm
+#echo "#!/bin/bash
+#export NVM_DIR=\"/usr/local/nvm\"
+#[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"
+#npm \$@" > /usr/local/bin/npm
+#chmod +x /usr/local/bin/npm
 
 
 echo " install postgres "
 
-sudo apt-get install -y postgresql-9.5 postgresql-contrib-9.5 
+sudo apt-get install -y postgresql-9.6 postgresql-contrib-9.6 
 
-sudo pg_dropcluster --stop 9.5 main
-sudo pg_createcluster --locale en_US.UTF-8 --start 9.5 main
+sudo pg_dropcluster --stop 9.6 main
+sudo pg_createcluster --locale en_US.UTF-8 --start 9.6 main
 
 
 echo " attempting to setup postgres "
 
-PGMAIN=/etc/postgresql/9.5/main
+PGMAIN=/etc/postgresql/9.6/main
 PGUSER=vagrant
 PGPASS=vagrant
 
