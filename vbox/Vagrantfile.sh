e57af41a3d4eb2b9a5da44e322be86d082986bc2
vagrant <vagrant@ubuntu1810.localdomain>
2019-04-02 22:38:08 -0700

    readonly

--- vbox/Vagrantfile.sh
+++ vbox/Vagrantfile.sh
@@ -47,17 +47,32 @@ echo 'wal_level=hot_standby' >> $PGMAIN/postgresql.conf
 echo 'synchronous_commit = off' >> $PGMAIN/postgresql.conf
 echo 'work_mem = 128MB' >> $PGMAIN/postgresql.conf
 
+echo "hostssl dstore readonly 0.0.0.0/0 md5" >> $PGMAIN/pg_hba.conf
+
 echo '#HAXTBH' >> $PGMAIN/pg_hba.conf
 echo 'local replication all peer' >> $PGMAIN/pg_hba.conf
 /etc/init.d/postgresql restart
 
 sudo -u postgres bash -c "psql -c \"CREATE USER $PGUSER WITH SUPERUSER PASSWORD '$PGPASS';\""
+sudo -u postgres bash -c "psql -c \"CREATE USER readonly WITH LOGIN PASSWORD 'secret' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL 'infinity';\""
 
 sudo -u postgres createdb dstore
+
 sudo -u postgres bash -c "psql -c \"CREATE EXTENSION IF NOT EXISTS citext;\" dstore "
 sudo -u postgres psql -c "ALTER DATABASE dstore OWNER TO $PGUSER;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE dstore TO $PGUSER;"
+sudo -u postgres psql --dbname=dstore -c "GRANT ALL PRIVILEGES ON DATABASE dstore TO $PGUSER;"
+
+sudo -u postgres psql --dbname=dstore -c "REVOKE CREATE ON SCHEMA public FROM public;"
+
+sudo -u postgres psql --dbname=dstore -c "GRANT CONNECT ON DATABASE dstore TO readonly;"
+sudo -u postgres psql --dbname=dstore -c "GRANT USAGE ON SCHEMA public TO readonly;"
+sudo -u postgres psql --dbname=dstore -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;"
+
+sudo -u postgres psql --dbname=dstore -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;"
+
 sudo -u postgres psql -l
+
+
 
 #
 # more things to do on the live server...
@@ -69,3 +84,8 @@ sudo -u postgres psql -l
 
 # apparently this one is also vitally important in sysctl
 # vm.overcommit_memory = 2
+
+# create a readonly user
+
+
+
