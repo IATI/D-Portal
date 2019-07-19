c33d6787ac0ce580aaa17062820a0ad4dce7fe62
kriss <kriss@xixs.com>
2019-07-20 00:51:55 +0100

    .

--- dfetch/js/cmd.js
+++ dfetch/js/cmd.js
@@ -3,39 +3,51 @@
 
 var cmd=exports;
 
-var pfs=require("pify")( require("fs") )
-
-var jml=require("./jml.js")
-var xson=require("./xson.js")
-
-var stringify = require('json-stable-stringify');
-
-
+var util=require("util")
 var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
 
 
+cmd.parse=function(argv)
+{
+
+
+
+argv.dir   =           argv.dir   || process.env.DFETCH_DIR   || "downloaded"   ;
+argv.limit = parseInt( argv.limit || process.env.DFETCH_LIMIT || "999999"     ) ;
+
+}
+
 cmd.run=async function(argv)
 {
-	if( argv._[0]=="download" )
+	if( argv._[0]=="prepare" )
 	{
-		await require("./dfetch.js").download_all()
+		await require("./dfetch.js").download_prepare(argv)
 		return
 	}
 
 	// help text
 	console.log(
 		"\n"+
-		">	dfetch download \n"+
-		"Download all the iati data.\n"+
+		">	dfetch prepare \n"+
+		"Prepare a directory to fetch IATI pakages into.\n"+
+		"\n"+
+		"	--dir downloaded \n"+
+		"	Directory to download into.\n"+
+		"\n"+
+		"	--limit 999999 \n"+
+		"	Maximum number of files to download.\n"+
 		"\n"+
 		"\n"+
 	"");
 }
+
+
 
 // if global.argv is set then we are inside another command so do nothing
 if(!global.argv)
 {
 	var argv = require('yargs').argv
 	global.argv=argv
+	cmd.parse(argv)
 	cmd.run(argv)
 }
--- dfetch/js/dfetch.js
+++ dfetch/js/dfetch.js
@@ -1,32 +1,46 @@
 // Copyright (c) 2019 International Aid Transparency Initiative (IATI)
 // Licensed under the MIT license whose full text can be found at http://opensource.org/licenses/MIT
 
-var download=exports;
+var dfetch=exports;
 
-var dflat=require("./dflat.js")
+var util=require("util")
+var path=require("path")
+var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
 
 var pfs=require("pify")( require("fs") )
-var jml=require("./jml.js")
-var xson=require("./xson.js")
 var stringify = require('json-stable-stringify');
 
+var request=require('request');
 
-dfetch.download_all=async function()
+// I promise to turn a url into data
+var getbody=require("pify")( function(url,cb)
 {
-	await dfetch.download_meta()
-	await dfetch.download_data()
-}
+	request(url, function (error, response, body) {
+		if(error) { cb(error,null); }
+		else      { cb(null,body);  }
+	});
+} );
 
 
-dfetch.download_meta=async function()
+dfetch.download_prepare=async function(argv)
 {
-	const download = require('download')
+	console.log("Preparing \""+argv.dir+"\" to fetch upto "+argv.limit+" IATI packages.")
 
-}
+	if(! await pfs.existsSync(argv.dir) ){ await pfs.mkdirSync(argv.dir) } // create output directory
 
+	var body=JSON.parse( await getbody("https://iatiregistry.org/api/3/action/package_search?rows="+argv.limit) )
+	var results=body.result.results
 
-dfetch.download_data=async function()
-{
-	const download = require('download')
+	await pfs.writeFile( path.join(argv.dir,"all.meta.json") , stringify( results , {space:" "} ) )
+
+	for(var idx in results)
+	{
+		var result=results[idx]
+		var slug=result.name
+		var url=result.resources[0].url
+		ls(slug+" : "+url)
+
+		await pfs.writeFile( path.join(argv.dir,slug+".meta.json") , stringify( result , {space:" "} ) )
+	}
 
 }
