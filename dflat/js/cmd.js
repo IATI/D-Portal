0a51a729886cc17920c859577acd80134d53a4e2
kriss <kriss@xixs.com>
2019-10-17 18:00:36 +0100

    .

--- dflat/js/cmd.js
+++ dflat/js/cmd.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Copyright (c) 2019 International Aid Transparency Initiative (IATI)
 // Licensed under the MIT license whose full text can be found at http://opensource.org/licenses/MIT
 
