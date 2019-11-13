552743181870e7037df7214f7285cb036960c58a
kriss <kriss@xixs.com>
2019-11-13 00:55:04 +0000

    prepare for new savi

---
+++ ctrack/js/view_savi.js
@@ -0,0 +1,96 @@
+// Copyright (c) 2014 International Aid Transparency Initiative (IATI)
+// Licensed under the MIT license whose full text can be found at http://opensource.org/licenses/MIT
+
+
+var view_savi=exports;
+exports.name="view_savi";
+
+var ctrack=require("./ctrack.js")
+var plate=require("./plate.js")
+var iati=require("./iati.js")
+var fetch=require("./fetch.js")
+
+var dflat=require("../../dflat/js/dflat.js")
+var dflat_savi=require("../../dflat/js/savi.js")
+
+
+// the chunk names this view will fill with new data
+view_savi.chunks=[
+	"view_savi_file",
+];
+
+view_savi.view=function(args)
+{
+	ctrack.setcrumb(0);
+	ctrack.change_hash();
+
+	view_savi.ajax();
+}
+
+//
+// Perform all ajax calls to get data 
+//
+view_savi.ajax=function(args)
+{
+	args=args||{}
+	fetch.ajax_dat_fix(args)
+
+	if(args.aid)
+	{
+		view_savi.ajax_aid(args.aid)
+	}
+	else
+	if(args.pid)
+	{
+		view_savi.ajax_pid(args.pid)
+	}
+}
+
+
+view_savi.ajax_aid=function(aid)
+{
+	var dat={
+		"from":"xson",
+		"root":"/iati-activities/iati-activity",
+		"aid":aid,
+	}
+
+	fetch.ajax(dat,function(iati)
+	{
+		dflat.clean(iati) // clean this data
+		
+		dflat_savi.prepare(iati) // prepare for display
+
+		dflat_savi.chunks.iati=iati
+
+		var dd=dflat_savi.plate('<style>{css}</style><div>{iati./iati-activities/iati-activity:iati-activity||}</div>')
+
+		ctrack.chunk("view_savi_file",dd);
+
+		ctrack.display();
+	})
+}
+
+view_savi.ajax_pid=function(pid)
+{
+	var dat={
+		"from":"xson",
+		"root":"/iati-organisations/iati-organisation",
+		"pid":pid,
+	}
+
+	fetch.ajax(dat,function(iati)
+	{
+		dflat.clean(iati) // clean this data
+		
+		dflat_savi.prepare(iati) // prepare for display
+
+		dflat_savi.chunks.iati=iati
+
+		var dd=dflat_savi.plate('<style>{css}</style><div>{iati./iati-organisations/iati-organisation:iati-organisation}</div>')
+
+		ctrack.chunk("view_savi_file",dd);
+
+		ctrack.display();
+	})
+}
