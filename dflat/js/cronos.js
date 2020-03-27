ec7ade02000a5a35fa707d486b5365cd7ac3d052
kriss <kriss@xixs.com>
2020-03-27 19:11:57 +0000

    require shelljs

--- dflat/js/cronos.js
+++ dflat/js/cronos.js
@@ -6,16 +6,59 @@ var cronos=exports;
 const assert = require('assert')
 const path = require('path')
 
-var fs=require('fs')
+var pfs=require("pify")( require("fs") )
 var util=require('util');
 var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
 
 var stringify = require('json-stable-stringify');
 
+
+const JSON5 = require('json5')
+
 /*#js.dflat.pull
 
 */
-cronos.pull = function(){
+cronos.pull = async function(argv){
+
+	console.log("CRONOS : "+argv.cronos)
+	
+	let json_name = path.join(argv.cronos , "cronos.json" )
+	let json_data = await pfs.readFile(json_name,{ encoding: 'utf8' })
+	let config = JSON5.parse(json_data) // sloppy json parse for handwritten files
+
+	console.log(stringify(config))
+
+/*
+
+# init
+
+git clone --no-checkout --reference . . q0 && cd q0 && git checkout --orphan q0
+
+
+# pull
+
+rm -rf q0 && git clone --reference . --branch q0 . q0
+
+
+# update
+
+cd q0
+mkdir downloads && mkdir packages && wget http://d-portal.org/q.xml?title_like=%25COVID-19%25 -O downloads/activities.xml
+
+../../dflat packages activities --dir cronos/q0
+cp activities/*.xml .
+rm -rf downloads && rm -rf packages && rm -rf activities && rm -rf reporting-orgs
+
+
+# push
+
+cd q0
+git add *.xml && git push --set-upstream origin q0
+cd ..
+git push --all
+
+*/
+
 
 
 
@@ -24,7 +67,7 @@ cronos.pull = function(){
 /*#js.dflat.help
 
 */
-cronos.help = function(){
+cronos.help = async function(argv){
 	console.log(
 `
 >	dflat cronos
