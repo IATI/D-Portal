0c3dc99aa1ef42fdb40a10e16676a6771a872a4b
kriss <kriss@xixs.com>
2020-04-09 19:11:00 +0100

    prevent nulls in title or description

--- dstore/js/dstore_db.js
+++ dstore/js/dstore_db.js
@@ -697,9 +697,11 @@ dstore_db.refresh_act = function(db,aid,xml,head){
 		});
 
 
-		t.title=refry.tagval_narrative(act,"title");
-		t.description=refry.tagval_narrative(act,"description");				
-		t.reporting=refry.tagval_narrative(act,"reporting-org");				
+// remove null from these 
+		t.title=refry.tagval_narrative(act,"title") || "";
+		t.description=refry.tagval_narrative(act,"description") || "";
+
+		t.reporting=refry.tagval_narrative(act,"reporting-org");
 		t.reporting_ref=refry.tagattr(act,"reporting-org","ref");
 		t.status_code=tonumber(refry.tagattr(act,"activity-status","code"));
 
