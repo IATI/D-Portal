7c5b611bf767081fa6ce428991c4b3e102b379d5
kriss <kriss@xixs.com>
2020-04-16 16:37:52 +0100

    .

--- dflat/js/query.js
+++ dflat/js/query.js
@@ -101,6 +101,7 @@ query.serv = async function(req,res,next){
 				else
 				if(form=="xml")
 				{
+console.log(ret)
 					var x=jml.to_xml( xson.to_jml(ret) )
 					res.set('charset','utf8'); // always utf8
 					res.set('Content-Type', 'text/xml');
