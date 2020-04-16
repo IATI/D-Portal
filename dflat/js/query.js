bb936c442f12fbb0c9829d7f5369fd7fb14953d5
kriss <kriss@xixs.com>
2020-04-16 15:49:10 +0100

    alternative forms from dquery

--- dflat/js/query.js
+++ dflat/js/query.js
@@ -6,6 +6,9 @@ var query=exports;
 var util=require('util');
 var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
 
+var dflat=require('./dflat.js');
+var savi=require('./savi.js');
+var jml=require('./jml.js');
 
 
 var monitor = require("pg-monitor");
@@ -41,6 +44,8 @@ query.serv = async function(req,res,next){
 	else
 */	{
 		let sql=req.body.sql||req.query.sql
+		let form=(req.body.form||req.query.form||"json").toLowerCase()
+		let root=(req.body.root||req.query.root||"").toLowerCase()
 		if(sql) // a post query
 		{
 //			console.log( req.body.sql )
@@ -53,9 +58,72 @@ query.serv = async function(req,res,next){
 			})
 			var ending=new Date().getTime()
 			ret.duration=(ending-starting)/1000.0
-			res.set('charset','utf8');
-			res.set('Content-Type', 'application/json');
-			res.jsonp(ret);
+			
+			res.set('charset','utf8'); // always utf8
+
+			if(form=="json") // normal json
+			{
+				res.set('Content-Type', 'application/json');
+				res.jsonp(ret);
+			}
+			else // special format
+			{
+				
+				let tab=[]
+				let df={}
+
+				if(root)
+				{
+					df[root]=tab
+				}
+				else // raw xson table of results
+				{
+					df=tab
+				}
+		 
+				
+				// yank xson only out of result
+				for(var i=0;i<ret.result.length;i++)
+				{
+					var it=r.rows[i].xson
+					if( "string" == typeof it ) { it=JSON.parse( it ) } // this converts from string for sqlite niceness
+					tab.push( it )
+					
+				}
+
+				if(form=="csv")
+				{
+					var csv=dflat.xson_to_xsv(df,root,{root:true})
+					res.set('Content-Type', 'text/csv');
+					res.end(csv);
+				}
+				else
+				if(form=="xml")
+				{
+					var x=jml.to_xml( xson.to_jml(df) )
+					res.set('Content-Type', 'text/xml');
+					res.write(	'<?xml version="1.0" encoding="UTF-8"?>\n' )
+					res.end(x);
+				}
+				else
+				if(form=="html")
+				{
+					dflat.clean(df) // clean this data
+					savi.prepare(df) // prepare for display
+					savi.chunks.iati=df
+					var x=savi.plate(
+`<!DOCTYPE html>
+<html>
+<head>
+<script src="/savi/lib/savi.js" type="text/javascript" charset="utf-8"></script>
+<script> require("savi").start({ embeded:true }); </script>
+</head>
+<body><style>{savi-page-css}{savi-css}</style><div>{iati./iati-activities/iati-activity:iati-activity||}{iati./iati-organisations/iati-organisation:iati-organisation||}</div></body>
+`)
+					res.set('Content-Type', 'text/html');
+					res.end(x);
+				}
+			}
 		}
 		else
 		{
