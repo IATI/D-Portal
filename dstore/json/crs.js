21896aa33d2c636bbb8aadba48251fc8e431759d
kriss <kriss@xixs.com>
2021-02-16 13:41:44 +0000

    change default crs year to 2019

--- dstore/json/crs.js
+++ dstore/json/crs.js
@@ -1,7 +1,7 @@
 
 // this file is now the only place you need to update the CRS year for new data
 
-exports.year=2018
-exports.donors=require("./crs_2018.json")
-exports.sectors=require("./crs_2018_sectors.json")
+exports.year=2019
+exports.donors=require("./crs_2019.json")
+exports.sectors=require("./crs_2019_sectors.json")
 
