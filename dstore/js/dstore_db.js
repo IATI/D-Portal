cd0c5f97164bcdde7b1d566a6340fb002e90df08
kriss <kriss@xixs.com>
2022-03-27 20:32:53 +0100

    enable @ref indexing

--- dstore/js/dstore_db.js
+++ dstore/js/dstore_db.js
@@ -198,7 +198,7 @@ for( let pn in dflat_database.paths ) // auto xson indexes
 	{
 		let a=p.jpath[ p.jpath.length-1 ]
 // do not index any path ending in "" as it can be huge text, eg narratives
-		if( (a!="") && (a!="@ref") ) // ignore long strings
+		if( (a!="") /*&& (a!="@ref")*/ ) // ignore long strings
 		{
 			dflat_indexs[ a ] = p // merge
 		}
