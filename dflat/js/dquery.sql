30850e09ee884ae44a8829e621d4189095a2d4b0
shi <notshi@gmail.com>
2024-01-23 21:58:43 +0000

    update dquery examples

--- dflat/js/dquery.sql
+++ dflat/js/dquery.sql
@@ -1,86 +1,164 @@
 
 #^sql_select_count
+--/* Display the total number of activities in the database
 
 SELECT
 
-count(*)
-as count
+count(*) AS count
 
-FROM xson where root='/iati-activities/iati-activity'
+FROM xson WHERE root='/iati-activities/iati-activity'
 
-limit 10;
+LIMIT 1;
+
 
 #^sql_select_activity
+--/* Display a selection of 10 activities
 
 SELECT
 
 *
 
-FROM xson where root='/iati-activities/iati-activity'
+FROM xson WHERE root='/iati-activities/iati-activity'
 
-limit 10;
+LIMIT 10;
+
+
+#^sql_display_org_names
+--/* Display all the names for a participating-org published by reporting-org
+
+SELECT
+
+xson->>'@ref' AS "@ref" ,
+xson->'/narrative'->0->>'' AS "/narrative" ,
+pid as "reporting-org",
+count(*) AS count
+
+FROM xson WHERE root='/iati-activities/iati-activity/participating-org' 
+AND xson->>'@ref'='XM-DAC-47066'
+
+GROUP BY 1,2,3
 
 
 #^sql_select_organisation
+--/* Select an organisation by their @ref identifier
 
 SELECT
 
 *
 
-from xson where root='/iati-organisations/iati-organisation'
+FROM xson WHERE root='/iati-organisations/iati-organisation'
 
-and pid = 'GB-GOV-1'
+AND pid = 'GB-GOV-1'
 
-limit 1;
+LIMIT 1;
 
 
 #^sql_select_activity_top_level
+--/* Display top level elements and attributes for an activity by removing a selection of elements
 
 SELECT
 
-root,aid,pid,
-(xson - '{"/budget","/sector","/transaction","/description","/related-activity","/activity-date","/participating-org","/recipient-country","/planned-disbursement","/country-budget-items/budget-item","/location","/contact-info","/document-link","/default-aid-type","/policy-marker","/crs-add/other-flags","/result","/other-identifier","/conditions/condition","/recipient-region"}'::text[] )
-as xson
+root,
+aid,
+pid,
+( xson - '
+    {
+        "/budget",
+        "/sector",
+        "/transaction",
+        "/description",
+        "/related-activity",
+        "/activity-date",
+        "/participating-org",
+        "/recipient-country",
+        "/planned-disbursement",
+        "/country-budget-items/budget-item",
+        "/location",
+        "/contact-info",
+        "/document-link",
+        "/default-aid-type",
+        "/policy-marker",
+        "/crs-add/other-flags",
+        "/result",
+        "/other-identifier",
+        "/conditions/condition",
+        "/recipient-region"
+        
+    }'::TEXT[]
+) AS xson
 
-from xson where root='/iati-activities/iati-activity'
+FROM xson WHERE root='/iati-activities/iati-activity'
 
-limit 10;
+LIMIT 1;
+
 
 #^sql_select_subarray
+--/* Display elements within elements that can occur multiple times
+--/* This example lists all titles of document links in an activity
 
 SELECT
 
 *
 
-from ( SELECT aid , jsonb_array_elements(xson->'/title/narrative') as xson 
-from ( SELECT aid , jsonb_array_elements(xson->'/document-link') as xson
-from xson where root='/iati-activities/iati-activity'
-) as xson2
-) as xson3
+FROM
+(
+    SELECT
+    aid ,
+    jsonb_array_elements(xson->'/title/narrative') AS xson
+    
+    FROM
+    (
+        SELECT
+        aid ,
+        jsonb_array_elements(xson->'/document-link') AS xson
+        
+        FROM xson WHERE root='/iati-activities/iati-activity'
+        AND aid='CA-3-S061266PRG'
+    
+    ) AS xson2
+    
+) AS xson3
 
-limit 10;
+LIMIT 100;
+
 
 #^sql_select_documents_title_and_url
+--/* Display elements within elements that can occur multiple times
+--/* This example lists the titles and url of document links found in activities
 
 SELECT
 
 *
 
-from ( SELECT aid , jsonb_array_elements(xson->'/title/narrative')->'' as title , xson->'@url'as url
-from ( SELECT aid , jsonb_array_elements(xson->'/document-link') as xson
-from xson where root='/iati-activities/iati-activity'
-) as xson2
-) as xson3
+FROM
+(
+    SELECT
+    aid ,
+    jsonb_array_elements(xson->'/title/narrative')->'' AS title ,
+    xson->'@url' AS url
 
-limit 100;
+    FROM
+    (
+        SELECT
+        aid ,
+        jsonb_array_elements(xson->'/document-link') AS xson
+    
+        FROM xson WHERE root='/iati-activities/iati-activity'
+    
+    ) AS xson2
+    
+) AS xson3
+
+LIMIT 100;
 
 
 #^sql_count_document_links
 --/* Find activities with most document links at top level
+--/* Does not include document links in transactions
 
 SELECT
 
-aid AS "Activity Identifier",
+aid AS "Activity",
 
 ( 
     SELECT count(*) AS "Document Links"
@@ -97,19 +175,25 @@ LIMIT 20;
 
 
 #^sql_top_organic_tags
+--/* Display the list of codes used in the tag element ordered by the most
 
 SELECT
 
-xson->>'@code' as code , count(*)
+xson->>'@code' AS "@code",
+count(*) AS count
 
-FROM xson where root='/iati-activities/iati-activity/tag' and xson->>'@vocabulary' = '99' and xson->>'@vocabulary-uri' is null
+FROM xson WHERE root='/iati-activities/iati-activity/tag'
+AND xson->>'@vocabulary' = '99'
+AND xson->>'@vocabulary-uri' IS NULL
 
-group by 1 order by 2 desc
+GROUP BY 1
+ORDER BY 2 DESC
 
 limit 1000;
 
 
 #^sql_covid_19
+--/* COVID-19 search
 
 SELECT DISTINCT aid FROM xson WHERE
 (
@@ -144,11 +228,13 @@ SELECT DISTINCT aid FROM xson WHERE
 
 
 #^sql_root_list
+--/* Display the number of instances for elements that occur multiple times
 
-SELECT root , count(*)
+SELECT root , count(*) AS count
 
 FROM xson
 
 GROUP BY 1 ORDER BY 2 DESC
 
 LIMIT 1000;
+
