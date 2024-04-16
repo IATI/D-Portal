d3bc5fa70c045c213456fa03192a8beff678745d
kriss <kriss@xixs.com>
2024-04-16 20:09:54 +0100

    .

--- ctrack/js/view_main.js
+++ ctrack/js/view_main.js
@@ -68,12 +68,16 @@ view_main.view=function(args)
 		}
 	}});
 
+/*
 	views.list_participating_orgs.ajax({output:"count",limit:5,chunk:"list_participating_orgs_datas",callback:function(data){
 		if( (!data) || (!data.rows) || (data.rows.length==0) )
 		{
 			ctrack.chunk("main_participating_org","")
 		}
 	}});
+*/
+
+			ctrack.chunk("main_participating_org","")
 
 	var top_opts={}
 
