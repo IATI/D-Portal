9c48d10f55cd7bc9994b8894360a197522836fbb
kriss <kriss@xixs.com>
2025-02-18 18:07:31 +0000

    org map

--- ctrack/js/view_related.js
+++ ctrack/js/view_related.js
@@ -147,8 +147,7 @@ view_related.ajax=async function(args)
 	else
 	if(args.pid) // org
 	{
-//		view_related.ajax_pid(args.pid)
-		ctrack.chunk("related_datas","{related_data_error}")
+		view_related.ajax_pid(args.pid)
 	}
 	else
 	{
@@ -156,6 +155,273 @@ view_related.ajax=async function(args)
 	}
 
 }
+
+view_related.ajax_pid=async function(_pid)
+{
+	ctrack.chunk("related_aid","")
+	ctrack.chunk("related_pid",_pid)
+
+let q={}
+	q.pid=_pid // beware template tag expansion of pid
+	let pid="${pid}"
+	q.sql=`
+
+--$pid=NL-KVK-41177588
+WITH
+relatedp AS (
+    SELECT
+    a.reporting_ref as pid,
+    b.reporting_ref as related_pid,
+    r.related_type  as related_type
+
+    FROM related r
+    JOIN act a ON a.aid=r.aid
+    JOIN act b ON b.aid=r.related_aid
+    WHERE a.reporting_ref!=b.reporting_ref
+    GROUP BY a.reporting_ref,b.reporting_ref,r.related_type
+)
+
+
+SELECT g.pid,g.related_pid,g.depth,g.related_type
+	,xson->'/name/narrative'->0->'' AS name
+ FROM (
+
+WITH RECURSIVE
+graph1(pid, related_pid, related_type, depth) AS (
+    SELECT r.pid, r.related_pid, r.related_type, -1
+    FROM relatedp r WHERE
+        r.pid=${pid}
+        AND
+        related_type=1
+    UNION ALL
+        SELECT r.pid, r.related_pid, r.related_type, g.depth - 1
+        FROM relatedp r, graph1 g
+        WHERE r.pid = g.related_pid AND r.related_type=g.related_type
+) CYCLE pid SET is_cycle USING path
+,
+graph2(pid, related_pid, related_type, depth) AS (
+    SELECT r.pid, r.related_pid, r.related_type, 1
+    FROM relatedp r WHERE
+        r.pid=${pid}
+        AND
+        related_type=2
+    UNION ALL
+        SELECT r.pid, r.related_pid, r.related_type, g.depth + 1
+        FROM relatedp r, graph2 g
+        WHERE r.pid = g.related_pid AND r.related_type=g.related_type
+) CYCLE pid SET is_cycle USING path
+,
+graph3(pid, related_pid, related_type, depth) AS (
+    SELECT r.pid, r.related_pid, r.related_type, 0
+    FROM relatedp r WHERE
+        r.pid=${pid}
+        AND
+        related_type=ANY ('{3,4,5}'::int[])
+)
+,
+graph4(pid, related_pid, related_type, depth) AS (
+    WITH p(pid) AS (
+        SELECT r.related_pid FROM relatedp r WHERE
+        r.pid=${pid}
+        AND related_type=1
+    )
+    SELECT ${pid}, r.related_pid, 3, 0
+    FROM p, relatedp r WHERE
+        r.pid=p.pid
+        AND
+        related_type=2
+)
+
+SELECT * FROM graph1
+UNION
+SELECT * FROM graph2
+UNION
+SELECT * FROM graph3
+UNION
+SELECT * FROM graph4
+UNION
+SELECT ${pid},${pid},3,0
+
+) g
+
+LEFT JOIN xson x ON x.pid=g.related_pid AND root='/iati-organisations/iati-organisation'
+
+ORDER BY g.depth,g.related_type,g.pid,g.related_pid
+
+
+`;
+
+	let result=await fetcher.ajax(q)
+
+//	console.log(result)
+
+	let idx=1
+	let depth_min=0
+	let depth_max=0
+	let depths={}
+	for( let row of result.rows ) // probe min max
+	{
+		let d=Number(row.depth)
+		if( d<depth_min ) { depth_min=d }
+		if( d>depth_max ) { depth_max=d }
+	}
+	for( let depth=depth_min ; depth<=depth_max ; depth++ ) { depths[depth-depth_min]=[] }
+	for( let row of result.rows ) // build array of arrays
+	{
+		let depth=Number(row.depth)
+		row.idx=idx++
+		depths[depth-depth_min].push(row)
+	}
+	// remove all dupes carefully
+	let pids={}
+	let ds=[]
+	for( let depth=-1 ; depth>=depth_min ; depth-- ) { ds.push(depth) }
+	for( let depth= 1 ; depth<=depth_max ; depth++ ) { ds.push(depth) }
+	ds.push(0)
+	for( let depth of ds)
+	{
+		let rows=depths[depth-depth_min]
+		for( let r=rows.length-1 ; r>=0 ; r-- )
+		{
+			if( pids[ rows[r].related_pid ] )
+			{
+				rows.splice(r, 1)
+			}
+			else
+			{
+				pids[ rows[r].related_pid ]=true
+			}
+		}
+	}
+
+	let related_data=[]
+	let lookup={}
+
+	for( let depth=depth_min ; depth<=depth_max ; depth++ )
+	{
+		let rows=depths[depth-depth_min]
+		if(rows.length<=0) { continue }
+
+		let a={}
+		a.depth=depth
+		a.title="STREAM"
+		if( depth<0 )
+		{
+			a.title="Up Stream ("+(0-depth)+")"
+		}
+		else
+		if(depth==0)
+		{
+			a.title="Side Stream"
+		}
+		else
+		{
+			a.title="Down Stream ("+(depth)+")"
+		}
+		a.tab=[]
+		related_data.push(a)
+		for(let row of rows)
+		{
+			let it={}
+			it.lcount=0
+			it.rcount=0
+			it.idx=row.idx
+			it.depth=depth
+// fake aid for now
+//			it.aid=row.related_pid
+			it.pid=row.related_pid
+			it.title=row.name || it.pid
+
+			it.pivot=""
+			if( it.pid==_pid ) // pivot
+			{
+				it.pivot="related_pivot"
+			}
+
+			it.downs=[]
+			it.upups=[]
+			for( let di in depths)
+			{
+				for( let row of depths[di] ) // build array of arrays
+				{
+					if(row.pid==it.pid)
+					{
+						if(row.related_type==1)
+						{
+							it.upups.push(row.idx)
+						}
+						else
+						if(row.related_type==2)
+						{
+							it.downs.push(row.idx)
+						}
+					}
+				}
+			}
+
+			a.tab.push(it)
+			lookup[it.idx]=it
+		}
+	}
+
+	let related_graph={}
+	let a=[]
+	related_graph.list=a
+
+	for(let t of related_data)
+	{
+		for(let r of t.tab)
+		{
+//			console.log(r)
+			let s1="L"
+			let s2="R"
+			if(r.depth<0)
+			{
+				if((-r.depth)%2==1)
+				{
+					s1="R"
+					s2="L"
+				}
+			}
+			if(r.depth>0)
+			{
+				if((r.depth%2)==1)
+				{
+					s1="R"
+					s2="L"
+				}
+			}
+			for(let idx of r.upups)
+			{
+				a.push([s1,idx,r.idx,r.depth-1])
+			}
+			for(let idx of r.downs)
+			{
+				a.push([s2,r.idx,idx,r.depth])
+			}
+		}
+	}
+
+	for(let a of related_graph.list)
+	{
+		it1=lookup[a[1]]
+		it2=lookup[a[2]]
+		let name=a[0].toLowerCase()+"count"
+		it1[name]++
+		it2[name]++
+	}
+
+
+
+	ctrack.chunk("related_graph",related_graph)
+	ctrack.chunk("related_data",related_data)
+	ctrack.chunk("related_datas","{relatedp_data_head}{related_data:relatedp_data_body}{relatedp_data_foot}")
+
+//	console.log(related_data)
+	ctrack.display();
+
+}
+
 
 view_related.ajax_aid=async function(_aid)
 {
