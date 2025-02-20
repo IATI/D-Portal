db9c35cd34296692b76bf9f86097616adff3bf43
kriss <kriss@xixs.com>
2025-02-20 16:27:06 +0000

    merge aid pid display code show dupes in red

--- ctrack/js/view_related.js
+++ ctrack/js/view_related.js
@@ -142,12 +142,12 @@ view_related.ajax=async function(args)
 
 	if(args.aid) // act
 	{
-		view_related.ajax_aid(args.aid)
+		view_related.ajax_id("aid",args.aid)
 	}
 	else
 	if(args.pid) // org
 	{
-		view_related.ajax_pid(args.pid)
+		view_related.ajax_id("pid",args.pid)
 	}
 	else
 	{
@@ -156,14 +156,21 @@ view_related.ajax=async function(args)
 
 }
 
-view_related.ajax_pid=async function(_pid)
+view_related.ajax_id=async function(name,id)
 {
 	ctrack.chunk("related_aid","")
-	ctrack.chunk("related_pid",_pid)
+	ctrack.chunk("related_pid","")
+	ctrack.chunk("related_"+name,id)
+
 
 let q={}
-	q.pid=_pid // beware template tag expansion of pid
-	let pid="${pid}"
+	q[name]=id
+	let pid="${pid}" // prevent template tag expansion
+	let aid="${aid}" // prevent template tag expansion
+
+if(name=="pid")
+{
+
 	q.sql=`
 
 --$pid=NL-KVK-41177588
@@ -237,187 +244,9 @@ ORDER BY g.depth,g.related_type,g.pid,g.related_pid
 
 `;
 
-	let result=await fetcher.ajax(q)
-
-//	console.log(result)
-
-	let idx=1
-	let depth_min=0
-	let depth_max=0
-	let depths={}
-	for( let row of result.rows ) // probe min max
-	{
-		let d=Number(row.depth)
-		if( d<depth_min ) { depth_min=d }
-		if( d>depth_max ) { depth_max=d }
-	}
-	for( let depth=depth_min ; depth<=depth_max ; depth++ ) { depths[depth-depth_min]=[] }
-	for( let row of result.rows ) // build array of arrays
-	{
-		let depth=Number(row.depth)
-		row.idx=idx++
-		depths[depth-depth_min].push(row)
-	}
-	// remove all dupes carefully
-	let pids={}
-	let ds=[]
-	for( let depth=-1 ; depth>=depth_min ; depth-- ) { ds.push(depth) }
-	for( let depth= 1 ; depth<=depth_max ; depth++ ) { ds.push(depth) }
-	ds.push(0)
-	for( let depth of ds)
-	{
-		let rows=depths[depth-depth_min]
-		for( let r=rows.length-1 ; r>=0 ; r-- )
-		{
-			if( pids[ rows[r].related_pid ] )
-			{
-				rows.splice(r, 1)
-			}
-			else
-			{
-				pids[ rows[r].related_pid ]=true
-			}
-		}
-	}
-
-	let related_data=[]
-	let lookup={}
-
-	for( let depth=depth_min ; depth<=depth_max ; depth++ )
-	{
-		let rows=depths[depth-depth_min]
-		if(rows.length<=0) { continue }
-
-		let a={}
-		a.depth=depth
-		a.title="STREAM"
-		if( depth<0 )
-		{
-			a.title="Up Stream ("+(0-depth)+")"
-		}
-		else
-		if(depth==0)
-		{
-			a.title="Side Stream"
-		}
-		else
-		{
-			a.title="Down Stream ("+(depth)+")"
-		}
-		a.tab=[]
-		related_data.push(a)
-		for(let row of rows)
-		{
-			let it={}
-			it.lcount=0
-			it.rcount=0
-			it.idx=row.idx
-			it.depth=depth
-// fake aid for now
-//			it.aid=row.related_pid
-			it.pid=row.related_pid
-			it.title=row.name || it.pid
-
-			it.pivot=""
-			if( it.pid==_pid ) // pivot
-			{
-				it.pivot="related_pivot"
-			}
-
-			it.downs=[]
-			it.upups=[]
-			for( let di in depths)
-			{
-				for( let row of depths[di] ) // build array of arrays
-				{
-					if(row.pid==it.pid)
-					{
-						if(row.related_type==1)
-						{
-							it.upups.push(row.idx)
-						}
-						else
-						if(row.related_type==2)
-						{
-							it.downs.push(row.idx)
-						}
-					}
-				}
-			}
-
-			a.tab.push(it)
-			lookup[it.idx]=it
-		}
-	}
-
-	let related_graph={}
-	let a=[]
-	related_graph.list=a
-
-	for(let t of related_data)
-	{
-		for(let r of t.tab)
-		{
-//			console.log(r)
-			let s1="L"
-			let s2="R"
-			if(r.depth<0)
-			{
-				if((-r.depth)%2==1)
-				{
-					s1="R"
-					s2="L"
-				}
-			}
-			if(r.depth>0)
-			{
-				if((r.depth%2)==1)
-				{
-					s1="R"
-					s2="L"
-				}
-			}
-			for(let idx of r.upups)
-			{
-				a.push([s1,idx,r.idx,r.depth-1])
-			}
-			for(let idx of r.downs)
-			{
-				a.push([s2,r.idx,idx,r.depth])
-			}
-		}
-	}
-
-	for(let a of related_graph.list)
-	{
-		it1=lookup[a[1]]
-		it2=lookup[a[2]]
-		let name=a[0].toLowerCase()+"count"
-		it1[name]++
-		it2[name]++
-	}
-
-
-
-	ctrack.chunk("related_graph",related_graph)
-	ctrack.chunk("related_data",related_data)
-	ctrack.chunk("related_datas","{relatedp_data_head}{related_data:relatedp_data_body}{relatedp_data_foot}")
-
-//	console.log(related_data)
-	ctrack.display();
-
 }
-
-
-view_related.ajax_aid=async function(_aid)
+else // aid
 {
-
-	ctrack.chunk("related_aid",_aid)
-	ctrack.chunk("related_pid","")
-
-let q={}
-	q.aid=_aid // beware template tag expansion of aid
-	let aid="${aid}"
 	q.sql=`
 
 --$aid=US-GOV-18-NE
@@ -485,6 +314,7 @@ SELECT ${aid},${aid},3,0
 
 
 `;
+}
 
 	let result=await fetcher.ajax(q)
 
@@ -500,23 +330,45 @@ SELECT ${aid},${aid},3,0
 		if( d<depth_min ) { depth_min=d }
 		if( d>depth_max ) { depth_max=d }
 	}
+	for( let depth=depth_min ; depth<=depth_max ; depth++ ) { depths[depth-depth_min]=[] }
 	for( let row of result.rows ) // build array of arrays
 	{
-		let depth=Number(row.depth)-depth_min
-		if(!depths[depth]) { depths[depth]=[] }
+		let depth=Number(row.depth)
 		row.idx=idx++
-		depths[depth].push(row)
+		depths[depth-depth_min].push(row)
 	}
-	// remove dupes in side stream
+	// flag dupes in side stream
 	let rows=depths[0-depth_min]
 	for(let r1=rows.length-1;r1>=0;r1--)
 	{
 		for(let r2=0;r2<r1;r2++)
 		{
-			if(rows[r1].related_aid==rows[r2].related_aid)
+			if(rows[r1]["related_"+name]==rows[r2]["related_"+name])
 			{
 				rows.splice(r1, 1)
 				break;
+			}
+		}
+	}
+	// remove all dupes carefully
+	let ids={}
+	let ds=[]
+	for( let depth=-1 ; depth>=depth_min ; depth-- ) { ds.push(depth) }
+	for( let depth= 1 ; depth<=depth_max ; depth++ ) { ds.push(depth) }
+	ds.push(0)
+	for( let depth of ds)
+	{
+		let rows=depths[depth-depth_min]
+		for( let r=rows.length-1 ; r>=0 ; r-- )
+		{
+			if( ids[ rows[r]["related_"+name] ] )
+			{
+				rows[r].dupe=true
+//				rows.splice(r, 1)
+			}
+			else
+			{
+				ids[ rows[r]["related_"+name] ]=true
 			}
 		}
 	}
@@ -526,6 +378,9 @@ SELECT ${aid},${aid},3,0
 
 	for( let depth=depth_min ; depth<=depth_max ; depth++ )
 	{
+		let rows=depths[depth-depth_min]
+		if(rows.length<=0) { continue }
+
 		let a={}
 		a.depth=depth
 		a.title="STREAM"
@@ -544,7 +399,6 @@ SELECT ${aid},${aid},3,0
 		}
 		a.tab=[]
 		related_data.push(a)
-		let rows=depths[depth-depth_min]
 		for(let row of rows)
 		{
 			let it={}
@@ -552,51 +406,67 @@ SELECT ${aid},${aid},3,0
 			it.rcount=0
 			it.idx=row.idx
 			it.depth=depth
-			it.aid=row.related_aid
-			it.pid=row.reporting_ref
-			it.title=row.title || it.aid
-			it.currency="USD"
-			it.date_start=row.day_start?fetcher.get_nday(row.day_start):"N/A"
-			it.date_end=row.day_end?fetcher.get_nday(row.day_end):"N/A"
-			it.reporting=iati_codes.publisher_names[row.reporting_ref] || row.reporting || row.reporting_ref || "N/A";
-
-			it.commitment=row.commitment || 0
-			it.pct=0;
-			if( row.commitment!=0 )
-			{
-				it.pct=Math.floor(0.5+100*(row.spend/row.commitment));
-				if(it.pct<0){it.pct=0;}
-				if(it.pct>100){it.pct=100;}
-			}
-			else
-			if( row.spend )
-			{
-				it.commitment=row.spend
-				it.pct=100
-			}
+			it.id=""
+			it.dupe=row.dupe&&"dupe"||""
 
 			it.pivot=""
-			if( it.aid==_aid ) // pivot
+			if( it[name]==id ) // pivot
 			{
 				it.pivot="related_pivot"
 			}
 
+			if(name=="pid")
+			{
+				it.pid=row.related_pid
+				if(!row.dupe) { it.id=it.pid }
+				it.title=row.name || it.pid
+			}
+			else // aid
+			{
+				it.aid=row.related_aid
+				if(!row.dupe) { it.id=it.aid }
+				it.pid=row.reporting_ref
+				it.title=row.title || it.aid
+				it.currency="USD"
+				it.date_start=row.day_start?fetcher.get_nday(row.day_start):"N/A"
+				it.date_end=row.day_end?fetcher.get_nday(row.day_end):"N/A"
+				it.reporting=iati_codes.publisher_names[row.reporting_ref] || row.reporting || row.reporting_ref || "N/A";
+
+				it.commitment=row.commitment || 0
+				it.pct=0;
+				if( row.commitment!=0 )
+				{
+					it.pct=Math.floor(0.5+100*(row.spend/row.commitment));
+					if(it.pct<0){it.pct=0;}
+					if(it.pct>100){it.pct=100;}
+				}
+				else
+				if( row.spend )
+				{
+					it.commitment=row.spend
+					it.pct=100
+				}
+			}
+
 			it.downs=[]
 			it.upups=[]
-			for( let di in depths)
+			if(!row.dupe)
 			{
-				for( let row of depths[di] ) // build array of arrays
+				for( let di in depths)
 				{
-					if(row.aid==it.aid)
+					for( let row of depths[di] ) // build array of arrays
 					{
-						if(row.related_type==1)
+						if(row[name]==it[name])
 						{
-							it.upups.push(row.idx)
-						}
-						else
-						if(row.related_type==2)
-						{
-							it.downs.push(row.idx)
+							if(row.related_type==1)
+							{
+								it.upups.push(row.idx)
+							}
+							else
+							if(row.related_type==2)
+							{
+								it.downs.push(row.idx)
+							}
 						}
 					}
 				}
@@ -649,16 +519,23 @@ SELECT ${aid},${aid},3,0
 	{
 		it1=lookup[a[1]]
 		it2=lookup[a[2]]
-		let name=a[0].toLowerCase()+"count"
-		it1[name]++
-		it2[name]++
+		let side=a[0].toLowerCase()+"count"
+		it1[side]++
+		it2[side]++
 	}
 
 
 
 	ctrack.chunk("related_graph",related_graph)
 	ctrack.chunk("related_data",related_data)
-	ctrack.chunk("related_datas","{related_data_head}{related_data:related_data_body}{related_data_foot}")
+	if(name=="pid")
+	{
+		ctrack.chunk("related_datas","{relatedp_data_head}{related_data:relatedp_data_body}{relatedp_data_foot}")
+	}
+	else // aid
+	{
+		ctrack.chunk("related_datas","{related_data_head}{related_data:related_data_body}{related_data_foot}")
+	}
 
 //	console.log(related_data)
 	ctrack.display();
