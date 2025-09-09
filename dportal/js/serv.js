25e6692c1a295cbaac338e2354ee6079282dac56
kriss <kriss@xixs.com>
2025-09-09 11:53:11 +0100

    fix cors with a wildcard fallback when origin is not in header

--- dportal/js/serv.js
+++ dportal/js/serv.js
@@ -33,30 +33,34 @@ app.use( express_fileupload() );
 // we need to do crap with OPTIONS for some sites to talk to us ?
 
 app.use(function(req, res, next) {
-    var oneof = false;
-    if(req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if(oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
+	var oneof = false;
+	if(req.headers.origin) {
+		res.header('Access-Control-Allow-Origin', req.headers.origin);
+		oneof = true;
+	}
+	else
+	{
+		res.header('Access-Control-Allow-Origin', "*"); // wildcard
+	}
+	if(req.headers['access-control-request-method']) {
+		res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+		oneof = true;
+	}
+	if(req.headers['access-control-request-headers']) {
+		res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+		oneof = true;
+	}
+	if(oneof) {
+		res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+	}
 
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-        res.send(200);
-    }
-    else {
-        next();
-    }
+	// intercept OPTIONS method
+	if (oneof && req.method == 'OPTIONS') {
+		res.send(200);
+	}
+	else {
+		next();
+	}
 });
 
 app.use(function(req, res, next) {
