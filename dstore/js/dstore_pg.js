f2bab6dedb5cb27ac880da546bdfba42593ddfb8
kriss <kriss@xixs.com>
2025-11-19 22:41:55 +0000

    .

--- dstore/js/dstore_pg.js
+++ dstore/js/dstore_pg.js
@@ -584,6 +584,7 @@ await ( await dstore_pg.open() ).tx( async db => {
 				}
 				xwalk( xtree ,"/iati-organisations/iati-organisation")
 
+				consoel.log( tables_replace_sql["xson"] )
 				for(let x of xs )
 				{
 					console.log("importing xson "+x.pid+" "+x.root)
