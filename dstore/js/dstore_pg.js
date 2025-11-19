9a748d5a5ad14ba672625f127860916cc2c0600f
kriss <kriss@xixs.com>
2025-11-19 23:41:09 +0000

    .

--- dstore/js/dstore_pg.js
+++ dstore/js/dstore_pg.js
@@ -595,11 +595,11 @@ await ( await dstore_pg.open() ).tx( async db => {
 				console.log("importing budgets from org file for "+pid)
 
 				await dstore_back.delete_from(db,"budget",{aid:pid});
-/*
+
 				for( let it of refry.all_tags(org,"total-budget")){ await dstore_db.refresh_budget(db,it,xtree,{aid:pid},0); }
 				for( let it of refry.all_tags(org,"recipient-org-budget")){ await dstore_db.refresh_budget(db,it,xtree,{aid:pid},0); }
 				for( let it of refry.all_tags(org,"recipient-country-budget")){ await dstore_db.refresh_budget(db,it,xtree,{aid:pid},0); }
-*/
+
 				await dstore_back.replace(db,"slug",{"aid":pid,"slug":slug});
 
 				delete deleteme[pid] // replaced so no need to delete
